50bd9c621f93f03674b26bdd8a3dd5c1ffdd4947
Vanderson Nunes <vann.nunes@gmail.com>
2017-06-15 15:13:01 -0300

    OK

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    @include('partials.favicon');
+    @include('partials.favicon')
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
